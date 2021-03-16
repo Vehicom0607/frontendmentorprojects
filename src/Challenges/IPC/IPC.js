0471885846a45158814c9a15bf1436d300f4efab
Vehicom0607 <60050686+Vehicom0607@users.noreply.github.com>
2021-03-16 14:12:22 -0400

    Made more, need to work on slider and some extra text.

--- src/Challenges/IPC/IPC.js
+++ src/Challenges/IPC/IPC.js
@@ -5,16 +5,18 @@ import classes from './IPC.module.css'
 class IPC extends Component {
     render() {
         return (
-            <div className={classes.background}>
+            <div className={classes.Background}>
                 <Container>
                     <div className={classes.Head}>
                         <h1 className={classes.Header}>Simple, traffic-based pricing</h1>
                         <p className={classes.Description}>Sign-up for our 30-day trial. No credit card required.</p>
                     </div>
-                    <Card>
+                    <Card className={classes.Card}>
                         <Card.Body>
-                            <Card.Text>100k PAGEVIEWS</Card.Text>
-                            <Card.Text><span>$16.00</span>/month</Card.Text>
+                            <div className={classes.CardHead}>
+                                <Card.Text className={classes.PageView}>100k PAGEVIEWS</Card.Text>
+                                <Card.Text className={classes.Pricing}><span className={classes.Price}>$16.00</span>/month</Card.Text>
+                            </div>
                             <div>slider</div>
                             <Card.Text>Montly Billing<span></span>Yearly Billing</Card.Text>
                         </Card.Body>
